ed96ba9d78b8733d967cb66d2186b95520a31863
Chafic Nehme <cnehm006@uottawa.ca>
2023-12-05 16:39:12 -0500

    Grinch Melanie

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
+// Snowflake creation function
+function createSnowflake() {
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    const size = Math.random() * 5 + 2; // Random size between 2px and 7px
+    snowflake.style.width = size + 'px';
+    snowflake.style.height = size + 'px';
+    snowflake.style.left = Math.random() * 100 + 'vw';
+    snowflake.style.animation = 'fall ' + (Math.random() * 5 + 5) + 's linear forwards';
+
+    document.getElementById('snow-container').appendChild(snowflake);
+
+    setTimeout(() => {
+        snowflake.remove();
+    }, (Math.random() * 5 + 5) * 1000); // Random duration between 5s and 10s
+}
+
+setInterval(createSnowflake, 200);
+
+// Personalized greeting functionality
 document.getElementById('submit-button').addEventListener('click', function () {
     var name = document.getElementById('name-input').value.trim();
     var greetingElement = document.getElementById('greeting');
+    var grinchImage = document.getElementById('grinch-image');
+    var grinchSound = document.getElementById('grinch-sound');
     
-    if (name) {
-        if (name.toLowerCase() === 'amy') {
-            greetingElement.textContent = "Merry Christmas, Am2oum! ;)";
-        } else {
-            greetingElement.textContent = 'Merry Christmas, ' + name + '!';
-        }
-        greetingElement.style.display = 'block';
-        document.getElementById('name-form').style.display = 'none';
+    if (name.toLowerCase() === 'melanie') {
+        grinchImage.style.display = 'block';
+        grinchSound.play();
+        setTimeout(function() {
+            grinchImage.style.display = 'none';
+        }, 4000); // Display the Grinch image for 4 seconds
     } else {
-        alert('Please enter your name.');
+        if (name) {
+            greetingElement.textContent = 'Merry Christmas, ' + name + '!';
+            greetingElement.style.display = 'block';
+            document.getElementById('name-form').style.display = 'none';
+        } else {
+            alert('Please enter your name.');
+        }
     }
-});
-
-document.body.addEventListener('mousemove', function (e) {
-    var spark = document.createElement('div');
-    spark.style.position = 'absolute';
-    spark.style.left = e.clientX + 'px';
-    spark.style.top = e.clientY + 'px';
-    spark.style.width = '5px';
-    spark.style.height = '5px';
-    spark.style.borderRadius = '50%';
-    spark.style.backgroundColor = 'gold';
-    spark.style.boxShadow = '0 0 10px gold';
-    spark.style.animation = 'spark 0.5s forwards';
-
-    document.body.appendChild(spark);
-
-    setTimeout(function () {
-        document.body.removeChild(spark);
-    }, 500);
 });
